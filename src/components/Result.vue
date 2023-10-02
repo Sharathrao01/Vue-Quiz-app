8d3dffe14af11c0abb1e6ac164b718528ef737de
Sharath S Rao <86926101+Sharathrao01@users.noreply.github.com>
2023-10-02 12:06:26 +0530

    Add files via upload

---
+++ src/components/Result.vue
@@ -0,0 +1,41 @@
+<script setup>
+    import {defineProps} from "vue"
+    import {RouterLink} from "vue-router"
+
+    const {numberOfCorrectAnswers, quizQuestionLength} = defineProps(["quizQuestionLength", "numberOfCorrectAnswers"])
+</script>
+
+<template>
+    <div class="results">
+        <p>Your Results...</p>
+        <h1>
+            {{numberOfCorrectAnswers}}/{{quizQuestionLength}}
+        </h1>
+        <RouterLink to="/" class="redirect">Go Back</RouterLink>
+    </div>
+</template>
+
+<style scoped>
+    .results {
+        text-align: center;
+        padding: 100px 0;
+    }
+
+    p {
+        font-size: 25px;
+    }
+
+    h1 {
+        font-size: 80px;
+    }
+
+    .redirect{
+        text-decoration: none;
+        font-size: x-large;
+        font-weight: bold;
+        color: red;
+        background-color:bisque;
+        padding: 10px;
+        margin-top: 30px;
+    }
+</style>
